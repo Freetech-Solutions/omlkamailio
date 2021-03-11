a570ec6134310cda781a84df5073be2e1ab8a998
lionite <felipe.macias@freetechsolutions.com.ar>
2021-03-11 11:24:42 -0300

    quito eliminacion de usuario omnileads y envars al desinstalar rpm

--- scripts/after_remove.sh
+++ scripts/after_remove.sh
@@ -4,14 +4,3 @@ echo "Removing kamailio symbolic links and folders"
 rm -rf /usr/sbin/kamcmd
 rm -rf /usr/sbin/kamctl
 rm -rf /opt/omnileads/kamailio
-existe=$(grep -c '^omnileads:' /etc/passwd)
-if [ $existe -eq 0 ]; then
-  echo ""
-else
-  echo "Deleting omnileads user"
-  userdel omnileads
-fi
-if [ -f /etc/profile.d/omnileads_envars.sh ]; then
-  echo "Deleting envars"
-  rm -rf /etc/profile.d/omnileads_envars.sh
-fi
